1403b550bef69fd9c1038e354d9d2e01fcd26bc0
DARK-SHHADOW <74013997+DARK-SHHADOW@users.noreply.github.com>
2021-04-13 11:21:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 // var year = 2000;
 // var date = 23;
-var result = 0;
+var result;
 var k;
 var flag = 0;
 // var month = 09;
 let btn = document.querySelector("#choose");
 btn.onclick = function () {
-    let date = document.querySelector("#datee").value;
-    let month = document.querySelector("#monthh").value;
-    let year = document.querySelector("#yearr").value;
+    var date = document.querySelector("#datee").value;
+    var month = document.querySelector("#monthh").value;
+    var year = document.querySelector("#yearr").value;
 
 
-    if (date == 29 && monyh == 2) {
+    if (date == 29 && month == 2) {
         if (year % 4 != 0) {
-            alert("THE DATE 29 FEB INS'T EXIST IN THIS YEAR");
+            alert("THE 29 FEB ISN'T EXIST IN THIS YEAR %d", year);
         }
+
     }
+
+
     k = year % 100;
-    result = k / 4;
-    result = result + date;
+    var x = k / 4;
+    var y = Math.floor(x);
+
+    result = y + date;
+    // alert(result);
+
     if (month == 1 || month == 10) {
         result = result + 1;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
-
         } else {
             flag = 0;
-            alert("PLEASE ENTER THE VALID DATA");
-
+            alert("PLEASE ENTER THE VALID DATA ");
         }
     }
     if (month == 2) {
         result = result + 4;
-        if (1 <= date >= 29) {
+        if (1 <= date && date <= 29) {
             flag = 1;
         } else {
             flag = 0;
-            alert("PLEASE ENTER THE VALID DATA");
-
+            alert("PLEASE ENTER THE VALID DATA ");
         }
     }
     if (month == 3) {
         result = result + 4;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
         } else {
             flag = 0;
@@ -51,7 +55,7 @@ btn.onclick = function () {
     }
     if (month == 11) {
         result = result + 4;
-        if (1 <= date >= 30) {
+        if (1 <= date && date <= 30) {
             flag = 1;
         } else {
             flag = 0;
@@ -60,7 +64,7 @@ btn.onclick = function () {
     }
     if (month == 4) {
         result = result + 0;
-        if (1 <= date >= 30) {
+        if (1 <= date && date <= 30) {
             flag = 1;
         } else {
             flag = 0;
@@ -69,7 +73,7 @@ btn.onclick = function () {
     }
     if (month == 7) {
         result = result + 0;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
         } else {
             flag = 0;
@@ -78,7 +82,7 @@ btn.onclick = function () {
     }
     if (month == 5) {
         result = result + 2;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
         } else {
             flag = 0;
@@ -87,7 +91,7 @@ btn.onclick = function () {
     }
     if (month == 6) {
         result = result + 5;
-        if (1 <= date >= 30) {
+        if (1 <= date && date <= 30) {
             flag = 1;
         } else {
             flag = 0;
@@ -96,7 +100,7 @@ btn.onclick = function () {
     }
     if (month == 8) {
         result = result + 1;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
         } else {
             flag = 0;
@@ -105,9 +109,8 @@ btn.onclick = function () {
     }
     if (month == 9) {
         result = result + 6;
-        if (1 <= date <= 30) {
+        if (1 <= date && date <= 30) {
             flag = 1;
-            // flag = 1;
         } else {
             flag = 0;
             alert("PLEASE ENTER THE VALID DATA ");
@@ -115,11 +118,16 @@ btn.onclick = function () {
     }
     if (month == 12) {
         result = result + 6;
-        if (1 <= date >= 31) {
+        if (1 <= date && date <= 31) {
             flag = 1;
         } else {
             flag = 0;
             alert("PLEASE ENTER THE VALID DATA ");
+        }
+    }
+    if (year % 4 == 0) {
+        if (month == 1 || month == 2) {
+            result = result - 1;
         }
     }
     if (year >= 2000) {
@@ -127,25 +135,34 @@ btn.onclick = function () {
     }
     result = result + k;
     result = result % 7;
+
     while (flag == 1) {
 
         switch (result) {
             case 1: alert("THE DAY IS -:SUNDAY:-");
                 break;
+
             case 2: alert("THE DAY IS -:MONDAY:-");
                 break;
+
             case 3: alert("THE DAY IS -:TUESDAY:-");
                 break;
+
             case 4: alert("THE DAY IS -:WEDNESDAY:-");
                 break;
+
             case 5: alert("THE DAY IS -:THURSDAY:-");
                 break;
+
             case 6: alert("THE DAY IS -:FRIDAY:-");
                 break;
+
             case 0: alert("THE DAY IS -:SATARDAY:-");
                 break;
+
         }
-        flag = 0;
+
+        break;
     }
 }
 // alert("hello world ");
